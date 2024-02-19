862ab6bc8060daccdc306d4b8276735160e0c2ae
Ayush2897 <149145831+Ayush2897@users.noreply.github.com>
2024-02-19 10:56:07 +0530

    Update and rename create_jira_ticket.py to create_jira_ticket_through_api.py
    .

--- create_jira_ticket_through_api.py
+++ create_jira_ticket_through_api.py
@@ -3,7 +3,7 @@ from requests.auth import HTTPBasicAuth
 import json
 
 url = "https://ayush28chopde.atlassian.net/rest/api/3/issue"
-api_token = "ATATT3xFfGF082SMTSXNkRtSE6neHWKQyQEYHI7w8ePNWM77ElR0KDQkUNEoJ6i1VYY-w7RHVxbDlBW515Vyug1-KoqWTYfDb6c4tXpaiJxs8yh_wfu_g72YpakNWQaReQDLo5XugAewCuZV78m0N48LnnOjQWLZF5tqshF3tcxhupxgCsQSV6c=635E7472"
+api_token = "<api_token>"
 
 auth = HTTPBasicAuth("ayush28chopde@gmail.com", api_token)
 
